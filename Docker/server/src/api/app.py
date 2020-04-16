c69a0af995d00cd778e3e5aeaca483082068f834
J999Ventura <devil_man_13@hotmail.com>
2020-04-16 19:20:05 +0100

    urgent

--- Docker/server/src/api/app.py
+++ Docker/server/src/api/app.py
@@ -3,4 +3,4 @@ from v1 import create_app
 if __name__ == '__main__':
     #	app.run(host='0.0.0.0', port=80, debug=True)
     app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
