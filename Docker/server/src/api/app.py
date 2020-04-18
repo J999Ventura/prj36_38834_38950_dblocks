3059e7b71fb62e9567710742ba61a7f20fa36eba
Heral <heralmukesh@gmail.com>
2020-04-18 18:34:52 +0100

    [DEVOLPMENT_USER_AUTH_v2] - modifications on .env file

--- Docker/server/src/api/app.py
+++ Docker/server/src/api/app.py
@@ -1,4 +1,7 @@
 from v1 import create_app
+#from dotenv import load_dotenv
+
+#load_dotenv('.env') #the path to your .env file (or any other file of environment variables you want to load)
 
 if __name__ == '__main__':
     #	app.run(host='0.0.0.0', port=80, debug=True)
--- Docker/server/src/api/v1/__init__.py
+++ Docker/server/src/api/v1/__init__.py
@@ -2,15 +2,16 @@ from flask import Flask
 from . import models,routes
 from v1.models import db,migrate
 from .commands import create_tables,create_users,create_roles
-from os import environ
 from .extensions import guard, mail
+from os import environ
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     #obter o nome do ambiente (dev, prod)
-    environment = environ.get('FLASK_ENV')
-    app.config.from_pyfile(environment + '.cfg') #alterar para ir buscar ao dicionario de acordo com a variavel em .env
-    app.config.from_pyfile('..\mail_config.cfg') #alterar para ir buscar ao dicionario de acordo com a variavel em .env
+    environment = environ.get('FLASK_ENV') #carregar o ficheiro .env na maquina de prod antes de tudo set -a; source ~/my-project-dir/.env; set +a
+    app.config.from_pyfile(environment + '.cfg') 
+    app.config.from_pyfile('..\mail_config.cfg')
+
     #routes and models
     from .routes import User_route, Api_route
     routes.init_app(app)
